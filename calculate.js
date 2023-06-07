0f84ec552759f7e5aa57d37e17d78cd6dab57fbf
dew06 <ransikadewmini06@gmail.com>
2023-06-07 07:41:10 +0530

    first commit

---
+++ calculate.js
@@ -0,0 +1,35 @@
+function sum(){
+    let fno=parseInt(document.getElementById('firstNumber').value);
+    let sno=parseInt(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').value=fno+sno;
+}
+
+function sub(){
+    let fno = parseInt(document.getElementById('firstNumber').value);
+    let sno = parseInt(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').value=fno-sno;
+}
+
+function div(){
+    let fno = parseInt(document.getElementById('firstNumber').value);
+    let sno = parseInt(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').value = fno/sno;
+
+}
+
+function mul(){
+    let fno = parseInt(document.getElementById('firstNumber').value);
+    let sno = parseInt(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').value = fno*sno;
+}
+
+function mod(){
+    let fno = parseInt(document.getElementById('firstNumber').value);
+    let sno = parseInt(document.getElementById('secondNumber').value);
+
+    document.getElementById('result').value = fno%sno;
+}
